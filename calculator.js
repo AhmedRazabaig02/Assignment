0827b99b52b8f12fc735932c6ad7485ddd744f7c
muhammadumer <183298438+AhmedRazabaig02@users.noreply.github.com>
2026-04-08 23:19:26 +0500

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,12 @@
+let num1=40
+let num2=5
+let op="/"
+if (op==="+"){
+    console.log(num1+num2)
+}else if (op==="-"){
+    console.log(num1-num2)
+}else if (op==="*"){
+    console.log(num1*num2)
+}else if (op==="/"){
+    console.log(num1/num2)
+}
